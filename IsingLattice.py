d742b65c63060686724e3e11d104a2c704dc2f52
chappertron <52535659+chappertron@users.noreply.github.com>
2024-03-03 15:26:18 +0000

    typing hints added to the functions

--- IsingLattice.py
+++ IsingLattice.py
@@ -1,7 +1,8 @@
+from typing import Tuple
 import numpy as np
 
-class IsingLattice:
 
+class IsingLattice:
     E = 0.0
     E2 = 0.0
     M = 0.0
@@ -12,27 +13,28 @@ class IsingLattice:
     def __init__(self, n_rows, n_cols):
         self.n_rows = n_rows
         self.n_cols = n_cols
-        self.lattice = np.random.choice([-1,1], size=(n_rows, n_cols))
+        self.lattice = np.random.choice([-1, 1], size=(n_rows, n_cols))
 
-    def energy(self):
+    def energy(self) -> float:
         "Return the total energy of the current lattice configuration."
         energy = 0.0
         return energy
 
-    def magnetisation(self):
+    def magnetisation(self) -> float:
         "Return the total magnetisation of the current lattice configuration."
         magnetisation = 0.0
         return magnetisation
 
-    def montecarlostep(self, T):
+    def montecarlostep(self, temp: float) -> Tuple[float, float]:
         # complete this function so that it performs a single Monte Carlo step
         energy = self.energy()
-        #the following two lines will select the coordinates of the random spin for you
+        # the following two lines will select the coordinates of the random spin for you
         random_i = np.random.choice(range(0, self.n_rows))
         random_j = np.random.choice(range(0, self.n_cols))
-        #the following line will choose a random number in the rang e[0,1) for you
+        # the following line will choose a random number in the rang e[0,1) for you
         random_number = np.random.random()
+        ...
 
-    def statistics(self):
+    def statistics(self) -> Tuple[int, float, float, float, float]:
         # complete this function so that it calculates the correct values for the averages of E, E*E (E2), M, M*M (M2), and returns them with Nsteps
-        return 
+        ...
