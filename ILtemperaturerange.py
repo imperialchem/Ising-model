87a61821282791c1fd676fee1778985700c70726
chappertron <52535659+chappertron@users.noreply.github.com>
2024-03-03 15:37:46 +0000

    formatting and imports

--- ILtemperaturerange.py
+++ ILtemperaturerange.py
@@ -1,12 +1,12 @@
-from IsingLattice import *
-from matplotlib import pylab as pl
+from IsingLattice import IsingLattice
+from matplotlib import pylab as plt
 import numpy as np
 
 n_rows = 8
 n_cols = 8
 il = IsingLattice(n_rows, n_cols)
 il.lattice = np.ones((n_rows, n_cols))
-spins = n_rows*n_cols
+spins = n_rows * n_cols
 runtime = 100000
 times = range(runtime)
 temps = np.arange(1.5, 3.0, 0.1)
@@ -24,24 +24,26 @@ for t in temps:
     energysq.append(aveE2)
     magnetisations.append(aveM)
     magnetisationsq.append(aveM2)
-    #reset the IL object for the next cycle
+    # reset the IL object for the next cycle
     il.E = 0.0
     il.E2 = 0.0
     il.M = 0.0
     il.M2 = 0.0
     il.n_cycles = 0
-fig = pl.figure()
-enerax = fig.add_subplot(2,1,1)
+fig = plt.figure()
+enerax = fig.add_subplot(2, 1, 1)
 enerax.set_ylabel("Energy per spin")
 enerax.set_xlabel("Temperature")
 enerax.set_ylim([-2.1, 2.1])
-magax = fig.add_subplot(2,1,2)
+magax = fig.add_subplot(2, 1, 2)
 magax.set_ylabel("Magnetisation per spin")
 magax.set_xlabel("Temperature")
 magax.set_ylim([-1.1, 1.1])
-enerax.plot(temps, np.array(energies)/spins)
-magax.plot(temps, np.array(magnetisations)/spins)
-pl.show()
+enerax.plot(temps, np.array(energies) / spins)
+magax.plot(temps, np.array(magnetisations) / spins)
+plt.show()
 
-final_data = np.column_stack((temps, energies, energysq, magnetisations, magnetisationsq))
+final_data = np.column_stack(
+    (temps, energies, energysq, magnetisations, magnetisationsq)
+)
 np.savetxt("8x8.dat", final_data)
